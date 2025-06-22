fb536d00278b8932288acaf5760d27d85f4b3c30
Ayushi05105 <ayushijaiswal8720@gmail.com>
2025-06-22 19:32:12 +0530

    updated committed

---
+++ basic/input.py
@@ -0,0 +1,14 @@
+#1st
+name = input('What is Your name? ')
+color = input('What is your favorite color? ')
+print(name +' likes '+ color)
+
+#2nd
+birth_year = input('Birth year: ')
+age = 2025 - int(birth_year)
+print(age)
+
+#3rd
+weight_lbs = input('What is your weight? ' )
+weight_in_kg= int(weight_lbs)*0.4535
+print(weight_in_kg)
