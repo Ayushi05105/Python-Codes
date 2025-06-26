c8a8d52eab12bae2ad9bdaba55df207bee898c6a
Ayushi05105 <ayushijaiswal8720@gmail.com>
2025-06-26 21:35:45 +0530

    updated committed

---
+++ basic/comparison.py
@@ -0,0 +1,9 @@
+temperature = 30
+if temperature >30:
+    print("It's a hot day")
+elif temperature < 30:
+    print("It's not hot day")
+elif temperature == 30:
+    print("It's normal day")    
+else:
+    print("It's not a hot day")    
---
+++ basic/logical_oper.py
@@ -0,0 +1,7 @@
+has_good_credit = False
+has_criminal_record = True
+if has_good_credit and has_criminal_record:
+    print("Eligible for loan")
+if has_good_credit or has_criminal_record:
+    print("Eligible for loan")
+
