4697bc77627c882a4586a2cfbe2b652044dc1aa2
Ayushi05105 <ayushijaiswal8720@gmail.com>
2025-06-30 19:18:27 +0530

    updated code

---
+++ basic/classes.py
@@ -0,0 +1,19 @@
+class Point:            # capital 1st letter of every word
+    def move(self):
+        print("move")
+
+    def draw(self):
+        print("draw")  
+
+
+point1 = Point() 
+point1.x =10
+point1.y = 20
+print(point1.x)  
+print(point1.y)         
+point1.draw()    
+    
+    
+    
+    
+    
