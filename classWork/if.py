d4c9fef7ba6e1a742543231095322aa580bafe7e
Ayushi05105 <ayushijaiswal8720@gmail.com>
2025-10-10 21:29:50 +0530

    updated code

---
+++ classWork/if.py
@@ -0,0 +1,10 @@
+price = 1000000
+good_credit = True
+if good_credit:
+    down_payment = 0.1 * price
+else:
+    down_payment = 0.2 * price
+
+#print (down_payment) 
+print(f"Down payment: ${down_payment}")   
+
